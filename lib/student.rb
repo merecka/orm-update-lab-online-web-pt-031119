261e145c6dc0253daa57abd4467739ba3af1da94
Alex Merecka <merecka@gmail.com>
2019-05-25 00:59:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,9 @@ class Student
   end
 
   def self.new_from_db(row)
-    new_student = Student.new
+    name = nil
+    grade = nil
+    new_student = Student.new(name, grade)
     new_student.id = row[0]
     new_student.name = row[1]
     new_student.grade = row[2]
